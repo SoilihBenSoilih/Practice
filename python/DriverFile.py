abe50f24e8dbf7298c55ecfe3b5be6ab60dea7d9
Soilih BEN SOILIH <s.bensoilih@gmail.com>
2021-04-02 20:00:51 -0600

    singleton

---
+++ python/DriverFile.py
@@ -0,0 +1,6 @@
+from Singleton import Singleton
+
+sing1 =  Singleton()
+sing2 =  Singleton()
+
+print(sing1 is sing2)
---
+++ python/Singleton.py
@@ -0,0 +1,9 @@
+class Singleton(object):
+
+    instance = None
+
+    #called on the creaction of an object
+    def __new__(cls):
+        if cls.instance is None:
+            cls.instance = object.__new__(cls)
+        return cls.instance
